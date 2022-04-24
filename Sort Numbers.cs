adc05b24e433f2cee24ecc7b33646340f0857258
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2022-04-24 19:56:31 +0200

    new solution

---
+++ Sort Numbers.cs
@@ -0,0 +1,28 @@
+﻿public class Kata
+{
+    public static int[] SortNumbers(int[] nums)
+    {
+        if (nums == null)
+
+        {
+            nums = new int[0];
+        }
+
+        else
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        int r = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = r;
+                    }
+                }
+            }
+        }
+        return nums;
+    }
+}
