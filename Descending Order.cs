b400842317845db765ae57c74fe25c41bb34f3bd
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2022-04-24 19:45:37 +0200

    new Solution

---
+++ Descending Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int DescendingOrder(int num)
+    {
+        // Bust a move right here
+        int i = 0;
+        int[] ar = new int[50];
+        while (num >= 10)
+        {
+            ar[i] = num % 10;
+            num /= 10;
+            i++;
+        }
+        ar[i] = num;
+        int t = 0;
+        for (int a = 0; a < i; a++)
+        {
+            for (int b = 0; b < i - a; b++)
+            {
+                if (ar[b + 1] > ar[b])
+                {
+                    t = ar[b + 1];
+                    ar[b + 1] = ar[b];
+                    ar[b] = t;
+                }
+            }
+        }
+        num = ar[0];
+        for (int j = 1; j <= i; j++)
+        {
+            num *= 10;
+            num += ar[j];
+        }
+        return num;
+    }
+}
