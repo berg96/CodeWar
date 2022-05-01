82019d006a78ed342ef7411de093d8928b1b9b84
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2022-05-01 16:33:47 +0200

    one more

---
+++ Bit Counting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Kata
+{
+    public static int CountBits(int n)
+    {
+        int cb = 0;
+        if (n > 0)
+            cb++;
+        while (n > 1)
+        {
+            if (n % 2 == 1)
+            {
+                cb++;
+            }
+            n /= 2;
+        }
+        return cb;
+    }
+}
