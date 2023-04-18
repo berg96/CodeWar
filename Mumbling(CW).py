5947b5f44064a338eb0aab3798ff85ce7d71adf5
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-18 22:55:56 +0200

    1st try

---
+++ Mumbling(CW).py
@@ -0,0 +1,17 @@
+#accum("abcd") -> "A-Bb-Ccc-Dddd"
+#accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+#accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+def accum(s):
+    new_string = ''
+    i = 0
+    for char in s:
+        new_string += char.upper
+        if i > 0 :
+            for j in range (i-1):
+                new_string += char.lower
+        i += 1
+        new_string += '-'
+    return new_string.pop(len(new_string)-1)
+
+print (accum("abcd"))
