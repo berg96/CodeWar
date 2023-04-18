b90074058fbfc75867a8d3a01cb5048b0645bbf3
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-18 21:23:55 +0200

    small fix

--- Counting Duplicates(CW).py
+++ Counting Duplicates(CW).py
@@ -9,8 +9,7 @@ def duplicate_count(text):
         'O':0,'P':0,'Q':0,'R':0,'S':0,'T':0,'U':0,'V':0,'W':0,'X':0,'Y':0,'Z':0,
         '0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0
     }
-    text.upper()
-    for char in text:
+    for char in text.upper():
         letters_numbers[char] += 1
     count = 0
     for char in letters_numbers:
