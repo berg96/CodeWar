bd4c3730eb97ba969b5099b524fb5b30a38cd7a5
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-18 23:10:43 +0200

    2nd try

--- Mumbling(CW).py
+++ Mumbling(CW).py
@@ -12,6 +12,6 @@ def accum(s):
                 new_string += char.lower()
         i += 1
         new_string += '-'
-    return new_string.replace(new_string[len(new_string)-2],'')
+    return new_string[:-1]
 
 print (accum("abcd"))
