5ab40e509b2bc15418a37a7035168727ac25c7fe
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-18 23:12:59 +0200

    small fix

--- Mumbling(CW).py
+++ Mumbling(CW).py
@@ -8,7 +8,7 @@ def accum(s):
     for char in s:
         new_string += char.upper()
         if i > 0 :
-            for j in range (i-1):
+            for j in range (i):
                 new_string += char.lower()
         i += 1
         new_string += '-'
