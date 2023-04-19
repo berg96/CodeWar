736894b7a802709faa180f48850311f678e0cd21
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-19 23:12:06 +0200

    2nd try

--- Replace With Alphabet Position(CW).py
+++ Replace With Alphabet Position(CW).py
@@ -5,9 +5,10 @@
 def alphabet_position(text):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     output = ''
+    text.lower
     for char in text:
-        if char.lower in alphabet:
-            output += str(alphabet.find(char.lower)) + ' '
+        if char in alphabet:
+            output += str(alphabet.find(char) + 1) + ' '
     return output[:-1]
 
 print (alphabet_position("The sunset sets at twelve o' clock."))
