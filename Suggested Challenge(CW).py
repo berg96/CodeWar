61e13beba6ceacf524ed39a5a1366cc07f0c12d5
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-20 21:00:29 +0200

    1st try

---
+++ Suggested Challenge(CW).py
@@ -0,0 +1,13 @@
+#There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+def find_uniq(arr):
+    num = arr[0]
+    for i in range(len(arr)):
+        if num != arr[i]:
+            if num != arr [i+1]:
+                n = num
+            else:
+                n = arr[i+1]
+    return n 
+
+print(find_uniq([ 1, 1, 1, 2, 1, 1 ]))
