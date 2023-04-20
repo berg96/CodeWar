8cdb2d09b7e520c6e7af98dd3558f51bdc6dc0f5
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-21 00:17:12 +0200

    7th try

--- Suggested Challenge(CW).py
+++ Suggested Challenge(CW).py
@@ -2,9 +2,8 @@
 
 def find_uniq(arr):
     n = arr[0]
-    for i in range(len(arr)-1):
-        if n != arr[i] and arr[i] != arr[i-1] and (arr[i] != arr[i+1] or arr[i] != arr[i-2]):
-                print(arr[i],i)
+    for i in range(len(arr)):
+        if n != arr[i] and arr[i] != arr[i-1] and (arr[i] != arr[i-2] or arr[i] != arr[i+1]):
                 n = arr[i]
     return n
 
