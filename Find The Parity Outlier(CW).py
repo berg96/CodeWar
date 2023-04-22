1750699386fad677f790324b10ff86bc343f1f48
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-22 22:22:03 +0200

    simpler solution

--- Find The Parity Outlier(CW).py
+++ Find The Parity Outlier(CW).py
@@ -27,4 +27,17 @@ def find_outlier(integers):
                 return integers[1]      # return 2nd
     return None
 
+def find_outlier_2(integers):
+    listEven = []
+    listOdd = []
+    for i in integers:
+        if i % 2 == 0:
+            listEven.append(i)
+        else:
+            listOdd.append(i)
+    if len(listEven) == 1:
+        return listEven[0]
+    else:
+    	return listOdd[0]
+
 print (find_outlier([5219247, -3082459, 8656945, -8793873, -4071953, 6525061, 1510671, -8592321, 3359607, 4195759, -503223, 1888563, 863083, -3236743, -2985261, 3745871, 154581, -1908113, 4321625, -9817633, 479115, 4222071, 6023495, 9925647, 5582505, -1725403, 9809709, -2767534, -7195115, -5652955, -3063767, 527495, -5504461, -7658717]))
