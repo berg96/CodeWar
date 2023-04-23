4a0b8da5bff136ae761daa73eb5a3027e14f13c6
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-23 20:58:00 +0200

    2nd try

--- The Hashtag Generator(CW).py
+++ The Hashtag Generator(CW).py
@@ -2,7 +2,9 @@ def generate_hashtag(s):
     words = s.split()
     new_string = '#'
     for word in words:
-        new_string += word
+        word.lower()
+        new_string += word.title()
     return new_string
 
 print(generate_hashtag('      Codewars'))
+print(generate_hashtag('CoDeWaRs is niCe'))
