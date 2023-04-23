2bc5553f7f603dc27fd10c16581b1b13166ca607
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-23 21:42:00 +0200

    simpler solution

--- The Hashtag Generator(CW).py
+++ The Hashtag Generator(CW).py
@@ -10,6 +10,13 @@ def generate_hashtag(s):
         return False
     else:
         return new_string
+    
+def generate_hashtag_2(s):
+    output = "#"
+    for word in s.split():
+        output += word.capitalize()
+    return False if (len(s) == 0 or len(output) > 140) else output
 
 print(generate_hashtag('      Codewars'))
 print(generate_hashtag('CoDeWaRs is niCe'))
+print(generate_hashtag_2('CoDeWaRs is niCe'))
