7f98fc96ddc02faacb008bdb921652f1e3ae7b07
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-04-27 22:47:10 +0200

    1st try

---
+++ Valid Braces(CW).py
@@ -0,0 +1,21 @@
+#Write a function that takes a string of braces, and determines if the order of the braces is valid. 
+#It should return true if the string is valid, and false if it's invalid.
+
+def valid_braces(string):
+    check = ''
+    for brace in string:
+        if brace == '(' or brace == '[' or brace == '{':
+            check += brace
+        else:
+            if (check[len(check)-1] == '(' and brace == ')') or (check[len(check)-1] == '[' and brace == ']') or   (check[len(check)-1] == '{' and brace == '}'):
+                check = check[:-1]
+            else:
+                return False
+    if len(check) != 0:
+        return False
+    else:
+        return True
+
+print (valid_braces("{}()[]"))
+print (valid_braces("([{}])"))
+print (valid_braces("(}"))
