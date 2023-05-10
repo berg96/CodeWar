812ebe9c6604a1b5d060f2cc260a995c463aef5b
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-10 12:50:02 +0200

    1st try

---
+++ Return Negative(CW).py
@@ -0,0 +1,12 @@
+#In this simple assignment you are given a number and have to make it negative. 
+#But maybe the number is already negative?
+
+def make_negative( number ):
+    if number < 0:
+        return number
+    elif number > 0:
+        return (0 - number)
+    else:
+        return 0
+    
+print (make_negative(5))
